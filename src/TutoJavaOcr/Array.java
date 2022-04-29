93cde342048d54409cd0613d82f9fee7c210a11b
KevRWB <kevinrenaultoff@gmail.com>
2022-04-29 10:10:09 +0200

    Tuto Java Open classroom Array, ArrayList (List), Set(HashSET) Map/HashMap

--- src/TutoJavaOcr/Array.java
+++ src/TutoJavaOcr/Array.java
@@ -15,25 +15,24 @@ public class Array {
         int[][] twoDArray = new int[][]{{1, 3, 4}, {4, 8, 9}, {9, 0, 5}};
         int value = twoDArray[1][1]; // 8
 
-            //-------------------Loop in 2D Arrays
-                //---------------Row Major Order
-                for(int i = 0; i < twoDArray.length; i++){
-                    for(int j = 0; j < twoDArray[0].length; j++){
-                        twoDArray[i][j] += 20;
-                    }
-                }
-                //--------------Column Major Order
-                int count;
-                for(int i = 0; i < twoDArray[0].length; i++){
-                    count = 0;
-                    for(int j = 0; j < twoDArray.length; j++){
-                        count += twoDArray[j][i];
-                    }
-                    System.out.println(count);
-                }
+        //-------------------Loop in 2D Arrays
+        //---------------Row Major Order
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[0].length; j++) {
+                twoDArray[i][j] += 20;
+            }
+        }
+        //--------------Column Major Order
+        int count;
+        for (int i = 0; i < twoDArray[0].length; i++) {
+            count = 0;
+            for (int j = 0; j < twoDArray.length; j++) {
+                count += twoDArray[j][i];
+            }
+            System.out.println(count);
+        }
 
         String arrayToString = Arrays.deepToString(twoDArray); //2D array in string
-        System.out.println(arrayToString);
 
         //--------------------------ArrayList
         List<Integer> numberList = new ArrayList<>();
@@ -47,7 +46,8 @@ public class Array {
 
         int numberListSize = numberList.size(); // 2
 
-        System.out.println(numberListSize);
+            //ArrayList asList
+            List<Integer> myList = Arrays.asList(1, 3, 5);
 
         //---------------------------SET & HASHSET
         Set<String> mySet = new HashSet<>();
@@ -58,8 +58,19 @@ public class Array {
         mySet.remove("Kevin");
         int mySetSize = mySet.size(); // 2
 
-        System.out.println(mySet);
-        }
-
         //---------------------------MAP & HashMap
+        Map<Integer, String> map = new HashMap<>(); // create Map --> Key values dictionnary / collection
+        map.put(1, "One");
+        map.put(2, "two");
+        map.put(3, "three");
+        System.out.println(map.get(1)); // get value --> "One"
+
+        //---------------------Set Keys as final variables
+        final int K4 = 4;       //Create final variables
+        final int K5= 5;
+        map.put(K4, "Four");    //add those final variables to map Map
+        map.put(K5, "Five");
+        map.remove(5); //remove 5, "Five" Key value
+        System.out.println(map.size());  //Print size of map Map
+    }
 }
